9047a675fe613a51a2f4ad8249179ee721cb43bf
HuangZilong <1183960905@qq.com>
2026-03-22 01:10:39 +0800

    feat(memory): export ConversationHistoryService from MemoryModule

--- src/memory/memory.module.ts
+++ src/memory/memory.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ShortTermMemoryService } from './services/short-term-memory.service';
-import { redisConfig } from '../config/configuration';
+import { ConversationHistoryService } from './services/conversation-history.service';
+import { MessageEntity } from '../database/entities/message.entity';
+import { redisConfig, memoryConfig } from '../config/configuration';
 
 @Module({
-  imports: [ConfigModule.forFeature(redisConfig)],
-  providers: [ShortTermMemoryService],
-  exports: [ShortTermMemoryService],
+  imports: [
+    ConfigModule.forFeature(redisConfig),
+    ConfigModule.forFeature(memoryConfig),
+    TypeOrmModule.forFeature([MessageEntity]),
+  ],
+  providers: [ShortTermMemoryService, ConversationHistoryService],
+  exports: [ShortTermMemoryService, ConversationHistoryService],
 })
 export class MemoryModule {}
