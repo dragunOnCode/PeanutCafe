1a0b1abebacbfeabd63818b19a2601f00a2ebc34
HuangZilong <1183960905@qq.com>
2026-03-24 23:20:31 +0800

    feat(orchestration): add workflow graph constants

---
+++ src/orchestration/graph/workflow.graph.ts
@@ -0,0 +1,7 @@
+export const WORKFLOW_NODES = {
+  HYDRATE: 'hydrate_session_state',
+  ROUTE: 'route_current_message',
+  RUN_TASK: 'run_next_task',
+} as const;
+
+export type WorkflowNodeName = typeof WORKFLOW_NODES[keyof typeof WORKFLOW_NODES];
